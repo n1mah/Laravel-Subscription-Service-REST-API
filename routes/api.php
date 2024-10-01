7df63d9ba8f1ff14d1151d081a6d6be050ea2292
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-01 14:56:59 +0330

    Feat: Add Plan Route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\v1\PlanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/plan',PlanController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
