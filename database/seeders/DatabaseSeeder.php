5a884f136bd19d595515a2a70da811d9b5ee8e4e
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-01 19:32:46 +0330

    Feat: Add Section Model With Migration & Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            PlanSeeder::class
+            PlanSeeder::class,
+            SectionSeeder::class
         ]);
 
     }
