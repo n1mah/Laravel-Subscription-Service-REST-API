e6a0bc268546ca3c051166e82d731b16fd8726f5
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-01 21:23:15 +0330

    Feat: Add Relations Models

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invoice extends Model
 {
@@ -12,4 +13,8 @@ class Invoice extends Model
         'subscription_id',
         'link'
     ];
+    public function subscription():BelongsTo
+    {
+        return $this->belongsTo(Subscription::class);
+    }
 }
--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subscription extends Model
 {
@@ -14,12 +15,16 @@ class Subscription extends Model
         'plan_id',
         'start_date',
         'end_date',
-        'status'
+        'status',
     ];
     public function user():BelongsTo{
         return $this->belongsTo(User::class);
     }
     public function plan():BelongsTo{
         return $this->belongsTo(Plan::class);
+    }
+    public function invoices():HasMany
+    {
+        return $this->hasMany(Invoice::class);
     }
 }
