110e70b71bb1f297d776f427f58bf31df59a772f
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-01 15:45:01 +0330

    Feat: Add Register Controller With Route

---
+++ app/Http/Controllers/v1/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends BaseController
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+              'name' => 'required',
+              'email' => 'required|email|unique:users',
+              'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = Hash::make($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken($request->userAgent())->plainTextToken;
+        $success['name'] =  $user->name;
+
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,15 @@
 
 use App\Http\Controllers\v1\LoginController;
 use App\Http\Controllers\v1\PlanController;
+use App\Http\Controllers\v1\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
-    Route::post('/login',LoginController::class);
     Route::get('/plan',PlanController::class);
-
+    Route::post('/login',LoginController::class);
+    Route::post('/register',RegisterController::class);
 });
 
 Route::get('/user', function (Request $request) {
