fe67f9f8dae57fdd3bc4446b7cce80208bb2c314
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-01 15:00:49 +0330

    Feat: Add Route Group For Version

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\v1\PlanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/plan',PlanController::class);
+
+Route::prefix('v1')->group(function () {
+    Route::get('/plan',PlanController::class);
+
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
