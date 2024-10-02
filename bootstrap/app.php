d3546cf9e41c18dd140d37f197a28c16193b9e5f
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-02 10:49:30 +0330

    Feat: Add Exceptions Response On bootstrap.app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->stopIgnoring(AuthenticationException::class);
+
+        $exceptions->render(function (AuthenticationException $exception, Request $request) {
+            //Unauthorized
+            return response()->json([
+                'message' => 'Unauthorized action',
+            ], 401);
+        });
     })->create();
