4ba9e1ed7b5ce735216aadf13b2cd8b32fae4d63
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-02 10:16:46 +0330

    Feat: Add Success Method in User Test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -8,5 +8,16 @@ use Tests\TestCase;
 class UserTest extends TestCase
 {
    use RefreshDatabase;
-
+    public function test_user_login_success(): void
+    {
+        $this->seed();
+        $response = $this->post('/api/v1/login',[
+            'email' => 'operator3@gmail.com',
+            'password' => 'Abcd1234'
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [],
+        ]);
+    }
 }
