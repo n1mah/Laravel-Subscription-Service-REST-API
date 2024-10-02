e4714d7f7a0aa208dea7240777b786211ca37627
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-02 10:01:57 +0330

    Feat: Add Plan Unit Test

---
+++ tests/Unit/PlanTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class PlanTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function show_plan(): void
+    {
+        $response = $this->get('/api/v1/plan');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [],
+        ]);
+    }
+}
