f233704636f642217d344961b4ed5e436a655a1a
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-02 11:10:52 +0330

    Feat: Add Login Test

---
+++ tests/Unit/LoginTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_user_login_success(): void
+    {
+        $this->seed();
+        $response = $this->post('/api/v1/login',[
+            'email' => 'operator3@gmail.com',
+            'password' => 'Abcd1234'
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data','token'
+        ]);
+
+    }
+
+    public function test_user_login_unauthorised(): void
+    {
+        $response = $this->post('/api/v1/login');
+        $response->assertStatus(401);
+        $response->assertJsonStructure([
+            'data' => [],
+            'status',
+        ]);
+    }
+}
