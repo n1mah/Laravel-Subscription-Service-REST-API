598ccdb9e63262a90b4d347d18a5ca0d07337c8e
Nima Heidari <nima.heidari.n@gmail.com>
2024-10-02 10:49:50 +0330

    Feat: Add Exceptions Response On bootstrap.app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->render(function (AuthenticationException $exception, Request $request) {
             //Unauthorized
             return response()->json([
-                'message' => 'Unauthorized action',
+                'message' => 'Unauthorized',
             ], 401);
         });
     })->create();
